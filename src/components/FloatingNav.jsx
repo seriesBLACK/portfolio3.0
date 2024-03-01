33692be53e0ea248752dc3a3885d3aa93c7d3859
Abd ul Walee Talal <aboodtalal51@gmail.com>
2024-03-01 17:53:30 +0300

    fixed contact button

--- src/components/FloatingNav.jsx
+++ src/components/FloatingNav.jsx
@@ -12,20 +12,17 @@ import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
 export const FloatingNav = () => {
   const navItems = [
     {
-      name: "Home",
       link: "hero",
       icon: <IconHome />
     },
     {
-      name: "About",
       link: "about",
       icon: <IconMessage />
     },
     {
-      name: "Projects",
       link: "projects",
       icon: <IconUser />
-    }
+    },
   ]
 
 
@@ -77,7 +74,7 @@ export const FloatingNav = () => {
               <span className="block sm:hidden">{navItem.icon}</span>
             </a>
           ))}
-          <a href={`#${navItems.link}`} className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+          <a href={'#contactSec'} className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
             <span>Contact</span>
             <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
           </a>
@@ -86,6 +83,3 @@ export const FloatingNav = () => {
     </div>
   );
 };
-
-//className={"relative p-1 dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"}
-//        className={"flex max-w-[300px]  border-2 border-blue-700  fixed top-3 inset-x-0 mx-auto dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] p-2  items-center justify-center space-x-3"}
